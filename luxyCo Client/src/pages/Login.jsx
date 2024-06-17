073f686338e0247b5ab3056821038b062985e33e
denigogov <122295314+denigogov@users.noreply.github.com>
2024-06-17 13:36:07 +0200

    ADD ALERT MESSAGE WHEN USER LOGIN

--- luxyCo Client/src/pages/Login.jsx
+++ luxyCo Client/src/pages/Login.jsx
@@ -4,8 +4,12 @@ import { useState } from 'react';
 import { useAuth } from '../helpers/Auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 import LoadingRing from '../components/reusableComponent/LoadingRing';
+import Swal from 'sweetalert2';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
+const ALERT_TITLE = import.meta.env.VITE_ALERT_TITLE;
+const ALERT_MESSAGE = import.meta.env.VITE_ALERT_MESSAGE;
+const ALERT_ICON = import.meta.env.VITE_ALERT_ICON;
 
 const Login = () => {
   const [username, setUsername] = useState('demo');
@@ -16,6 +20,17 @@ const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const auth = useAuth();
+
+  const succesMessageBtn = () => {
+    return Swal.fire({
+      title: ALERT_TITLE,
+      html: ALERT_MESSAGE,
+      position: 'center',
+      icon: ALERT_ICON,
+      iconColor: '#da0063',
+      confirmButtonColor: '#da0063',
+    });
+  };
 
   const redirectPath = location.state?.path || '/';
 
@@ -46,6 +61,10 @@ const Login = () => {
 
   const handleSubmit = async e => {
     e.preventDefault();
+
+    {
+      ALERT_TITLE.length > 4 && (await succesMessageBtn());
+    }
     const token = await loginResponse({
       username,
       password,
